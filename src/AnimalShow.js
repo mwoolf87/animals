65b8f5ada3de01b13346bac643c05d28e9a4fadb
mwoolf87 <michael.j.woolf@gmail.com>
2023-01-03 15:29:38 -0500

    created show component

---
+++ src/AnimalShow.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AnimalShow() {
+  return <div>Animal Show </div>;
+}
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AnimalShow from "./AnimalShow";
+
+const App = () => {
+  return (
+    <div>
+      <div>Show Animal List here!</div>
+      <AnimalShow />
+    </div>
+  );
+};
+
+export default App;
