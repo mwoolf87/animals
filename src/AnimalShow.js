3ef403355f095a5daa1656f57bcb9d7e434d8d79
mwoolf87 <michael.j.woolf@gmail.com>
2023-01-04 21:07:25 -0500

    added animal svg's and hooked to animal type in AnimalShow component

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,5 +1,25 @@
 import React from "react";
+import bird from "./svg/bird.svg";
+import dog from "./svg/dog.svg";
+import cow from "./svg/cow.svg";
+import cat from "./svg/cat.svg";
+import gator from "./svg/gator.svg";
+import heart from "./svg/heart.svg";
+import horse from "./svg/horse.svg";
 
-export default function AnimalShow() {
-  return <div>Animal Show </div>;
+const svgMap = {
+  bird,
+  dog,
+  cow,
+  cat,
+  gator,
+  horse
+};
+
+export default function AnimalShow({ type }) {
+  return (
+    <div>
+      <img src={svgMap[type]} alt="animal" />
+    </div>
+  );
 }
--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
 import AnimalShow from "./AnimalShow";
-
+import { useState } from "react";
 const App = () => {
+  const [animals, setAnimals] = useState([]);
+
+  const getRandomAnimal = () => {
+    const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+  };
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
   return (
     <div>
-      <div>Show Animal List here!</div>
-      <AnimalShow />
+      <button onClick={handleClick}>Add Animal</button>
+      <div>{renderedAnimals}</div>
     </div>
   );
 };
